41571ad67ce4386a43762224946190cce79de13e
bil-ash <33216854+bil-ash@users.noreply.github.com>
2024-05-19 06:40:50 +0530

    Update firebase-messaging-sw.js
    to 9.0.0

--- static/lib/firebase-messaging-sw.js
+++ static/lib/firebase-messaging-sw.js
@@ -2,8 +2,8 @@
 
 /* globals firebase, importScripts */
 
-importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
 
 firebase.initializeApp(/* config */);
 
