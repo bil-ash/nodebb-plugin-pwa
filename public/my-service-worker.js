dfd8a7340c5d45ffaabfd780314d78382bfcb14c
bil-ash <33216854+bil-ash@users.noreply.github.com>
2024-10-04 13:52:47 +0530

    Update my-service-worker.js
    4 cache strategies

--- public/my-service-worker.js
+++ public/my-service-worker.js
@@ -108,6 +108,65 @@ self.addEventListener('notificationclick', (event) => {
 	);
 });
 
+
+function cacheFirstFetch(event) {
+  event.respondWith(
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
+
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
+
+        return response;
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            // The following validates that the request was for a navigation to a new document
+            if (event.request.destination !== "document" || event.request.mode !== "navigate") {
+              return;
+            }
+
+            console.log("[PWA Builder] Network request failed and no cache." + error);
+            // Use the precached offline page as fallback
+            return caches.open(CACHE).then(function (cache) {
+              cache.match(offlineFallbackPage);
+            });
+          });
+      }
+    )
+  );
+}
+
+
+function networkFirstFetch(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+        return response;
+      })
+      .catch(function (error) {
+        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+}
+
 function fromCache(request) {
   // Check to see if you have it in the cache
   // Return response
